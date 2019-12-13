ed7c35312f34e97aae6a3b6c3c6e7a3f140e12c7
Yoann BENES <ybenes@gmail.com>
2019-12-13 15:51:54 +0100

    Manager Admin et read manager agent

--- v2/Modele/Manager/MAdmin.php
+++ v2/Modele/Manager/MAdmin.php
@@ -1,5 +1,21 @@
 <?php
+require_once("Modele/Admin.php");
 
-class           MAdmin{
-    
+class           MAdmin extends BDD
+{
+    public function getByLogin($login){
+        $res = $this->dbquery("SELECT * FROM `admin` WHERE `login` = '".$login."';");
+        $res = $res->fetchAll();
+        if (isset($res[0]['password'])){
+            $res = $res[0];
+            $admin = new Admin();
+            $admin-> set_id($res['id']);
+            $admin-> set_nom($res['nom']);
+            $admin-> set_prenom($res['prenom']);
+            $admin-> set_login($res['login']);
+            $admin-> set_password($res['password']);
+            return $admin;
+        }
+        return false;
+    }
 }
--- v2/Modele/Manager/MAgent.php
+++ v2/Modele/Manager/MAgent.php
@@ -1,5 +1,59 @@
 <?php
 
-class           MAgent{
-    
+class           MAgent extends BDD
+{
+
+    public function getByLogin($login)
+    {
+        $res = $this->dbquery("SELECT * FROM `agent` WHERE `login` = '" . $login . "';");
+        $res = $res->fetchAll();
+        if (isset($res[0]['id'])) {
+            $res = $res[0];
+            $agent = new Agent();
+            $agent->set_id($res['id']);
+            $agent->set_nom($res['nom']);
+            $agent->set_prenom($res['prenom']);
+            $agent->set_login($res['login']);
+            $agent->set_password($res['password']);
+            return $agent;
+        }
+        return false;
+    }
+
+    public function getById($id)
+    {
+        $res = $this->dbquery("SELECT * FROM `agent` WHERE `id` = '" . $id . "';");
+        $res = $res->fetchAll();
+        if (isset($res[0]['id'])) {
+            $res = $res[0];
+            $agent = new Agent();
+            $agent->set_id($res['id']);
+            $agent->set_nom($res['nom']);
+            $agent->set_prenom($res['prenom']);
+            $agent->set_login($res['login']);
+            $agent->set_password($res['password']);
+            return $agent;
+        }
+        return false;
+    }
+
+    public function getAll()
+    {
+        $res = $this->dbquery("SELECT * FROM `agent`;");
+        $res = $res->fetchAll();
+        if (isset($res[0]['id'])) {
+            $listAgent = array();
+            foreach ($res as $dbAgent) {
+                $agent = new Agent();
+                $agent->set_id($dbAgent['id']);
+                $agent->set_nom($dbAgent['nom']);
+                $agent->set_prenom($dbAgent['prenom']);
+                $agent->set_login($dbAgent['login']);
+                $agent->set_password($dbAgent['password']);
+                array_push($listAgent, $agent);
+            }
+            return $listAgent;
+        }
+        return false;
+    }
 }
