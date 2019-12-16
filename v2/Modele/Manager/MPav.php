776f880e71ae1e398347272723ad96a59813bcd2
Yoann BENES <ybenes@gmail.com>
2019-12-16 13:24:45 +0100

    modification des create des managers

--- v2/Modele/Manager/MPav.php
+++ v2/Modele/Manager/MPav.php
@@ -46,11 +46,23 @@ class               MPav extends BDD
     public function create($pav)
     {
         $this->dbquery("INSERT INTO `pav` (`id`, `numero`, `adresse`, `code_postal`, `ville`) VALUES (NULL, '" . $pav->get_numero() . "', '" . $pav->get_adresse() . "', '" . $pav->get_code_postal() . "', '" . $pav->get_ville() . "');");
-    }
+        $res = $this->dbquery("SELECT * FROM `pav` WHERE `id` = LAST_INSERT_ID();");
+        $res = $res->fetchAll();
+        if (isset($res[0]['id'])) {
+            $res = $res[0];
+            $pav = new Pav();
+            $pav->set_id($res['id']);
+            $pav->set_numero($res['numero']);
+            $pav->set_adresse($res['adresse']);
+            $pav->set_code_postal($res['code_postal']);
+            $pav->set_ville($res['ville']);
+            return $pav;
+        }
+     }
 
     public function update($pav)
     {
-        $this->dbquery("UPDATE `pav` SET `numero` = '" . $pav->get_numero() . "', `adresse` = '" . $pav->get_adresse() . "', `code_postal` = '" . $pav->get_code_postal() . "', `ville` = '" . $pav->get_ville() . "' WHERE `pav`.`id` = '" . $pav->get_id() . "';");
+        $res = $this->dbquery("UPDATE `pav` SET `numero` = '" . $pav->get_numero() . "', `adresse` = '" . $pav->get_adresse() . "', `code_postal` = '" . $pav->get_code_postal() . "', `ville` = '" . $pav->get_ville() . "' WHERE `pav`.`id` = '" . $pav->get_id() . "';");
     }
 
     public function delete($pav)
