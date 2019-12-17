80566733e84f8f16927a75f563192ad8934f11ce
Julien Frey <julien@les-gens.org>
2019-12-17 15:26:51 +0100

    formulaire adminagentedition

--- v2/View/AdminAgentCreation.php
+++ v2/View/AdminAgentCreation.php
@@ -1,8 +1,35 @@
 <?php
-$title = "PAV - Tournée";
+$title = "Création d'un nouvel Agent";
 ob_start();
 ?>
-Agent - Creation
+<h3>Agent - Creation</h3>
+<?php if (isset($validate) && $validate){?>
+    Nouvel agent crée.
+    
+    <button type="submit" href="?page=agentCreate">Retour</button>
+
+<?php } else {?>
+    <form action="?page=agentCreate" method="post">
+        <div>
+            <label for="nom"> Nom</label>
+            <input type="text" id="nom" name="nom" require>
+        </div>
+        <div>
+            <label for="prenom"> Prénom</label>
+            <input type="text" id="prenom" name="prenom" require>
+        </div>
+        <div>
+            <label for="login"> Login</label>
+            <input type="text" id="login" name="login" require>
+        </div>
+        <div>
+            <label for="password"> Mot de passe</label>
+            <input type="password" id="password" name="pwd" require>
+        </div>
+        
+        <button type="submit">Envoi</button>
+    </form>
+<?php } ?>
 <?php
 $content = $content . ob_get_contents();
 ob_clean();
