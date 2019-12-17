96dfda55a602d39bf8c345ed3f3753238df7d0a4
Yoann BENES <ybenes@gmail.com>
2019-12-17 16:15:29 +0100

    Edition de l'agent

--- v2/Modele/Manager/BDDParam.php
+++ v2/Modele/Manager/BDDParam.php
@@ -14,7 +14,7 @@ abstract class BDDParam{
         $this->dbHost = '127.0.0.1';
         $this->dbBase = 'mld_pav_v2';
         $this->dbUser = 'root';
-        $this->dbPwd = '';
+        $this->dbPwd = 'root';
         $this->dbCharset = 'utf8mb4';
         $this->dbOptions = [
             PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
