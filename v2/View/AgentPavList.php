3f2a7d54aa0ecc70e7564ff44914e4c1f05926dc
Yoann BENES <ybenes@gmail.com>
2019-12-19 14:50:35 +0100

    dsrtersgsd

--- v2/View/AgentPavList.php
+++ v2/View/AgentPavList.php
@@ -5,26 +5,34 @@ ob_start();
 ?>
 <table>
     <tr>
+        <th>Status</th>
         <th>Numero relevé</th>
         <th>Adresse</th>
         <th>Relever</th>
     </tr>
-<?php if (isset($noTournee)){
-echo "Pas de tournée prevue aujourd'hui";
-} else {
-    foreach ($releveList as $releve){
-        foreach ($pavList as $pav){
-            if($releve->get_id_pav() == $pav->get_id()){
-                $link = "?page=pavreleve&id_releve=".$releve->get_id();
-                ?> <tr>
-                    <td><?= $releve->get_id() ?></td>
-                    <td><?= $pav->get_numero()." ". $pav->get_adresse()." ".$pav->get_code_postal()." ".$pav->get_ville()?></td>
-                    <td><a href="<?= $link?>" >Relever</a></td>
-                </tr><?php
-            }
-        }
-    }
-?>
+    <?php if (isset($noTournee)) {
+        echo "Pas de tournée prevue aujourd'hui";
+    } else {
+        foreach ($releveList as $releve) {
+            foreach ($pavList as $pav) {
+                if ($releve->get_id_pav() == $pav->get_id()) {
+                    $link = "?page=pavreleve&id_releve=" . $releve->get_id();
+                    ?> <tr>
+                        <td><?php
+                                            if ($releve->get_status() == 's')
+                                                echo '<span style="color:lime;">Relevé';
+                                            else
+                                                echo '<span style="color:orange;">En attente';
+                                            ?></span>
+                        </td>
+                        <td><?= $releve->get_id() ?></td>
+                        <td><?= $pav->get_numero() . " " . $pav->get_adresse() . " " . $pav->get_code_postal() . " " . $pav->get_ville() ?></td>
+                        <td><a href="<?= $link ?>">Relever</a></td>
+                    </tr><?php
+                                        }
+                                    }
+                                }
+                                ?>
 </table>
 <?php } ?>
 
